4ae54e29e8ea77ab7395bba8d42e626e19ed4cf5
shinji5761 <shinji.tec.5761@gmail.com>
2025-06-23 03:02:59 +0900

    アイコンボタンにloading関係を追加

--- src/components/Common/IconButton.tsx
+++ src/components/Common/IconButton.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { ActivityIndicator, TouchableOpacity, View } from 'react-native';
 
 type Props = {
   icon: React.ReactNode;
   theme?: 'info' | 'danger' | 'warn' | 'theme' | 'background';
   onPress: () => void;
+  disabled?: boolean;
+  loading?: boolean;
 };
-export default function IconButton({ icon, theme = 'theme', onPress }: Props) {
+export default function IconButton({
+  icon,
+  theme = 'theme',
+  onPress,
+  disabled = false,
+  loading = false,
+}: Props) {
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity onPress={onPress} disabled={disabled}>
       <View
         className={`
         h-12 w-12 rounded-full
         flex justify-center items-center
         bg-light-${theme} dark:bg-dark-${theme}
-     `}
+        ${disabled ? 'opacity-50' : ''}
+        `}
       >
-        {icon}
+        {loading && <ActivityIndicator size="small" color="white" />}
+        {!loading && icon}
       </View>
     </TouchableOpacity>
   );
