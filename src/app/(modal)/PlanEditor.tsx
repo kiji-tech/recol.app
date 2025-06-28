f38ecf1947e4f03f1eaad55b7bff389803de6eba
shinji5761 <shinji.tec.5761@gmail.com>
2025-06-28 15:15:41 +0900

    要素に感覚がなかったため(スクロールも不要

--- src/app/(modal)/PlanEditor.tsx
+++ src/app/(modal)/PlanEditor.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from '@/src/components';
 import { BackgroundView } from '@/src/components';
 import { borderColor } from '@/src/themes/ColorUtil';
 import { useRouter } from 'expo-router';
-import { View, Text, TextInput, ScrollView, Alert } from 'react-native';
+import { View, Text, TextInput, Alert } from 'react-native';
 import * as Location from 'expo-location';
 import { useAuth } from '@/src/contexts/AuthContext';
 import { usePlan } from '@/src/contexts/PlanContext';
@@ -50,52 +50,50 @@ export default function PlanEditor() {
           router.back();
         }}
       />
-      <ScrollView>
-        {/* タイトル */}
-        <View className="w-full flex flex-col justify-start items-start">
-          <Text className={`text-lg font-bold text-light-text dark:text-dark-text`}>
-            計画の題目を入力してください｡
-          </Text>
-          <TextInput
-            value={plan?.title || ''}
-            placeholder="◯◯のお茶会..."
-            placeholderTextColor="gray"
-            className={`flex flex-row justify-center rounded-xl items-center border px-4 py-4 w-full text-xl
+      {/* タイトル */}
+      <View className="w-full flex flex-col justify-start items-start">
+        <Text className={`text-lg font-bold text-light-text dark:text-dark-text`}>
+          計画の題目を入力してください｡
+        </Text>
+        <TextInput
+          value={plan?.title || ''}
+          placeholder="◯◯のお茶会..."
+          placeholderTextColor="gray"
+          className={`flex flex-row justify-center rounded-xl items-center border px-4 py-4 w-full text-xl
                 ${borderColor} text-light-text dark:text-dark-text bg-light-background dark:bg-dark-background
                 `}
-            onChangeText={(text) =>
-              setPlan({ ...plan, title: text } as Tables<'plan'> & {
-                schedule: Tables<'schedule'>[];
-              })
-            }
-          />
-        </View>
-        <View className="w-full flex flex-col justify-start items-start">
-          <Text className="text-lg font-bold text-light-text dark:text-dark-text">メモ</Text>
-          <TextInput
-            value={plan?.memo || ''}
-            multiline={true}
-            placeholder="メモを入力してください｡"
-            placeholderTextColor="gray"
-            className={`rounded-xl border px-4 py-4 w-full text-lg h-32 text-start align-top 
+          onChangeText={(text) =>
+            setPlan({ ...plan, title: text } as Tables<'plan'> & {
+              schedule: Tables<'schedule'>[];
+            })
+          }
+        />
+      </View>
+      <View className="w-full flex flex-col justify-start items-start">
+        <Text className="text-lg font-bold text-light-text dark:text-dark-text">メモ</Text>
+        <TextInput
+          value={plan?.memo || ''}
+          multiline={true}
+          placeholder="メモを入力してください｡"
+          placeholderTextColor="gray"
+          className={`rounded-xl border px-4 py-4 w-full text-lg h-32 text-start align-top 
                         border-light-border dark:border-dark-border text-light-text dark:text-dark-text bg-light-background dark:bg-dark-background`}
-            onChangeText={(text) =>
-              setPlan({ ...plan, memo: text } as Tables<'plan'> & {
-                schedule: Tables<'schedule'>[];
-              })
-            }
-          />
-        </View>
-        <View>
-          <Text className="text-lg font-bold text-light-text dark:text-dark-text">
-            友達を追加する
-          </Text>
-          <Button theme="info" text="選択" onPress={() => alert('準備中')} />
-        </View>
-        <View className="w-full justify-center">
-          <Button theme="theme" text="登録する" onPress={handlerSubmit} />
-        </View>
-      </ScrollView>
+          onChangeText={(text) =>
+            setPlan({ ...plan, memo: text } as Tables<'plan'> & {
+              schedule: Tables<'schedule'>[];
+            })
+          }
+        />
+      </View>
+      <View>
+        <Text className="text-lg font-bold text-light-text dark:text-dark-text">
+          友達を追加する
+        </Text>
+        <Button theme="info" text="選択" onPress={() => alert('準備中')} />
+      </View>
+      <View className="w-full justify-center">
+        <Button theme="theme" text="登録する" onPress={handlerSubmit} />
+      </View>
     </BackgroundView>
   );
 }
