c67178aac5e153baa9fbca9864ae143855d0eac0
shinji5761 <shinji.tec.5761@gmail.com>
2025-06-28 16:01:50 +0900

    AI機能を一旦削除

--- src/components/GoogleMaps/BottomSheet/PlaceDetail.tsx
+++ src/components/GoogleMaps/BottomSheet/PlaceDetail.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BottomSheetScrollView } from '@gorhom/bottom-sheet';
-import { Place, Review } from '@/src/entities/Place';
+import { Place } from '@/src/entities/Place';
 import { Text, View, Linking } from 'react-native';
+import { useTheme } from '@/src/contexts/ThemeContext';
 import Header from '../../Header/Header';
 import RateViewer from './RateViewer';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import IconButton from '../../Common/IconButton';
-import { reviewAIAnalyze } from '@/src/apis/OpenAI';
-import Loading from '../../Loading';
 import ImageScrollView from '../../Common/ImageScrollView';
-import { useTheme } from '@/src/contexts/ThemeContext';
-import Button from '../../Common/Button';
+
 type Props = {
   place: Place;
   selected: boolean;
@@ -23,35 +21,35 @@ type Props = {
 export default function PlaceDetail({ place, selected, onAdd, onRemove, onClose }: Props) {
   // === Member ===
   const { isDarkMode } = useTheme();
-  const [isAiNavigation, setIsAiNavigation] = useState(false);
-  const [isAiLoading, setIsAiLoading] = useState(false);
-  const [aiText, setAiText] = useState('');
+  //   const [isAiNavigation, setIsAiNavigation] = useState(false);
+  //   const [isAiLoading, setIsAiLoading] = useState(false);
+  //   const [aiText, setAiText] = useState('');
 
   // === Method ===
   /** AIによるレビュー解析の結果取得 */
-  const fetchAiText = async () => {
-    setIsAiNavigation(true);
-    if (!place.reviews) {
-      setAiText('レビューがありません');
-      return;
-    }
-    setIsAiLoading(true);
+  //   const fetchAiText = async () => {
+  //     setIsAiNavigation(true);
+  //     if (!place.reviews) {
+  //       setAiText('レビューがありません');
+  //       return;
+  //     }
+  //     setIsAiLoading(true);
 
-    const reviews = place.reviews
-      .map((review: Review) => {
-        return `いつ： ${review.publishTime}
-評価：${review.rating}
-コメント：${review.text.text}
-        `;
-      })
-      .join('\n\n');
+  //     const reviews = place.reviews
+  //       .map((review: Review) => {
+  //         return `いつ： ${review.publishTime}
+  // 評価：${review.rating}
+  // コメント：${review.text.text}
+  //         `;
+  //       })
+  //       .join('\n\n');
 
-    // AI実行
-    reviewAIAnalyze(reviews).then((text: string | null) => {
-      setAiText(text || '');
-      setIsAiLoading(false);
-    });
-  };
+  //     // AI実行
+  //     reviewAIAnalyze(reviews).then((text: string | null) => {
+  //       setAiText(text || '');
+  //       setIsAiLoading(false);
+  //     });
+  //   };
 
   const handleAdd = () => {
     if (place) onAdd(place);
@@ -77,7 +75,7 @@ export default function PlaceDetail({ place, selected, onAdd, onRemove, onClose 
             }))}
         />
       )}
-      <View className="flex flex-col justify-start items-start p-4 gap-4  pb-40">
+      <View className="flex flex-col justify-start items-start p-4 gap-6  pb-40">
         {/* ロケーション名 */}
         <Header
           title={place.displayName.text}
@@ -107,37 +105,6 @@ export default function PlaceDetail({ place, selected, onAdd, onRemove, onClose 
         <Text className="text-ellipsis text-light-text dark:text-dark-text">
           {place.editorialSummary?.text || ''}
         </Text>
-
-        {/* AIレビュー */}
-        <Text className="text-xl font-semibold text-light-text dark:text-dark-text">
-          レビュー要約({place.reviews.length}件)
-        </Text>
-        {!isAiNavigation && (
-          <Button text="AIで店舗のレビューを要約する" theme="info" onPress={fetchAiText} />
-        )}
-        {isAiNavigation && !isAiLoading && (
-          <Text className={`text-light-text dark:text-dark-text`}>{aiText}</Text>
-        )}
-        {isAiNavigation && isAiLoading && (
-          <View className="w-full h-36">
-            <Loading />
-          </View>
-        )}
-
-        {/* 営業時間 */}
-        {place.currentOpeningHours && (
-          <Text className="text-xl font-semibold text-light-text dark:text-dark-text">
-            営業時間
-          </Text>
-        )}
-        {place.currentOpeningHours &&
-          place.currentOpeningHours.weekdayDescriptions.map((weekday) => (
-            <Text key={weekday} className="text-nowrap text-light-text dark:text-dark-text">
-              {weekday}
-            </Text>
-          ))}
-        {/* 予約 */}
-
         {/* ボタングループ */}
         <View className="flex flex-row justify-start items-center gap-4">
           {place.websiteUri && (
@@ -167,6 +134,36 @@ export default function PlaceDetail({ place, selected, onAdd, onRemove, onClose 
             />
           )}
         </View>
+
+        {/* AIレビュー */}
+        {/* <Text className="text-xl font-semibold text-light-text dark:text-dark-text">
+          レビュー要約({place.reviews.length}件)
+        </Text>
+        {!isAiNavigation && (
+          <Button text="AIで店舗のレビューを要約する" theme="info" onPress={fetchAiText} />
+        )}
+        {isAiNavigation && !isAiLoading && (
+          <Text className={`text-light-text dark:text-dark-text`}>{aiText}</Text>
+        )}
+        {isAiNavigation && isAiLoading && (
+          <View className="w-full h-36">
+            <Loading />
+          </View>
+        )} */}
+
+        {/* 営業時間 */}
+        {place.currentOpeningHours && (
+          <Text className="text-xl font-semibold text-light-text dark:text-dark-text">
+            営業時間
+          </Text>
+        )}
+        {place.currentOpeningHours &&
+          place.currentOpeningHours.weekdayDescriptions.map((weekday) => (
+            <Text key={weekday} className="text-nowrap text-light-text dark:text-dark-text">
+              {weekday}
+            </Text>
+          ))}
+        {/* 予約 */}
       </View>
     </BottomSheetScrollView>
   );
