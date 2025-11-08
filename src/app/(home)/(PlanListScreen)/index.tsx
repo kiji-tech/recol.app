fe42acce5aa6b8d01e09c8119271c52e145a15f0
kiji-tech <shinji.tec.5761@gmail.com>
2025-11-08 17:01:25 +0900

    update: PlanListScreenに並び替えのメニューを追加｡

--- src/app/(home)/(PlanListScreen)/index.tsx
+++ src/app/(home)/(PlanListScreen)/index.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { BackgroundView, Header } from '@/src/components';
-import IconButton from '@/src/components/IconButton';
-import { useFocusEffect, useRouter } from 'expo-router';
+import { useFocusEffect } from 'expo-router';
 import { useCallback } from 'react';
 import { View } from 'react-native';
 import { usePlan } from '@/src/contexts/PlanContext';
-import { useTheme } from '@/src/contexts/ThemeContext';
 import { LogUtil } from '@/src/libs/LogUtil';
 import ToastManager, { Toast } from 'toastify-react-native';
 import { Plan } from '@/src/features/plan/types/Plan';
 import NotFoundPlanView from '../../../features/plan/components/NotFoundPlanView';
 import PlanCard from '../../../features/plan/components/PlanCard';
+import PlanListMenu from '@/src/features/plan/components/PlanListMenu';
+
 
 export default function PlanListScreen() {
   // === Member ===
-  const router = useRouter();
-  const { isDarkMode } = useTheme();
   const { planList, fetchPlan, planLoading } = usePlan();
 
   // === Method ===
@@ -43,23 +40,9 @@ export default function PlanListScreen() {
     }, [])
   );
 
-  // === Render ===
-  /** プラン追加ボタン */
-  const addButton = () => {
-    return (
-      <IconButton
-        icon={<MaterialIcons name="add" size={18} color={isDarkMode ? 'white' : 'black'} />}
-        theme="info"
-        onPress={() => {
-          router.push('/(modal)/PlanCreator');
-        }}
-      />
-    );
-  };
-
   return (
     <BackgroundView>
-      <Header title="予定一覧" rightComponent={addButton()} />
+      <Header title="予定一覧" rightComponent={<PlanListMenu />} />
       {/* プランがない場合 */}
       {!planLoading && planList.length === 0 && <NotFoundPlanView />}
       {/* プラン一覧 */}
