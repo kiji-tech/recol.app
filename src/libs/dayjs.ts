22cdde771c8eca1ec47baef8ccf324152fad23de
kiji-tech <shinji.tec.5761@gmail.com>
2025-11-15 02:13:53 +0900

    fix: day.jsのlocales設定を変更

--- src/libs/dayjs.ts
+++ src/libs/dayjs.ts
@@ -4,12 +4,13 @@ import localizedFormat from 'dayjs/plugin/localizedFormat';
 // 使用するロケールをインポート
 import 'dayjs/locale/ja';
 import 'dayjs/locale/en';
+import { getLocales } from 'expo-localization';
 
 // localizedFormatを有効化
 dayjs.extend(localizedFormat);
 
 // ユーザーのロケールを取得
-const userLocale = navigator.language || 'ja';
+const userLocale = getLocales()[0].languageCode || 'en';
 
 // dayjsにロケールを設定
 dayjs.locale(userLocale);
