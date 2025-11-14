6376cef80d2bc6d04b7a88a88445d41982a849c7
kiji-tech <shinji.tec.5761@gmail.com>
2025-11-14 21:59:35 +0900

    fix: 多言語設定の追加

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -2,11 +2,15 @@ import { getLocales } from 'expo-localization';
 import { I18n } from 'i18n-js';
 import en from '../languages/en.json';
 import ja from '../languages/ja.json';
+import fr from '../languages/fr.json';
+import de from '../languages/de.json';
 
 // Set the key-value pairs for the different languages you want to support.
 const translations = {
   en: en,
   ja: ja,
+  fr: fr,
+  de: de,
 };
 const i18n = new I18n(translations);
 
