61c7712948b12db63747caac082ee439557321a5
kiji-tech <shinji.tec.5761@gmail.com>
2025-11-14 20:00:36 +0900

    fix: バリデーションチェック

--- supabase/functions/schedule/deleteSchedule.ts
+++ supabase/functions/schedule/deleteSchedule.ts
@@ -1,5 +1,5 @@
 import { Context } from 'jsr:@hono/hono';
-import { SupabaseClient, User } from 'npm:@supabase/supabase-js';
+import { SupabaseClient } from 'npm:@supabase/supabase-js';
 import { getMessage } from '../libs/MessageUtil.ts';
 import { LogUtil } from '../libs/LogUtil.ts';
 
@@ -10,13 +10,12 @@ import { LogUtil } from '../libs/LogUtil.ts';
  * @param user {User} 認証済みユーザー情報
  * @return {Promise<Response>} 削除結果を含むレスポンス
  */
-export const deleteSchedule = async (c: Context, supabase: SupabaseClient, user: User) => {
+export const deleteSchedule = async (c: Context, supabase: SupabaseClient) => {
   LogUtil.log('[DELETE] schedule/:id', { level: 'info' });
   const { uid } = await c.req.json();
-  if (!user) {
-    return c.json({ message: getMessage('C001'), code: 'C001' }, 403);
+  if (!uid) {
+    return c.json({ message: getMessage('C009', ['スケジュール']), code: 'C009' }, 400);
   }
-
   // 削除フラグの更新
   const { data, error } = await supabase
     .from('schedule')
