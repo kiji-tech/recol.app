c2ec88f208979526e8def0bae79686c41e049528
kiji-tech <shinji.tec.5761@gmail.com>
2025-12-04 20:23:22 +0900

    update: JWT関係でエラーになるので直接supabaseで実行

--- src/features/posts/apis/fetchPostsList.ts
+++ src/features/posts/apis/fetchPostsList.ts
@@ -1,17 +1,17 @@
-import { Posts } from '../types/Posts';
-
+import { supabase } from '@/src/libs/supabase';
 export async function fetchPostsList(
   option: { offset?: number; limit?: number },
   ctrl?: AbortController
 ) {
-  const response = await fetch(`${process.env.EXPO_PUBLIC_SUPABASE_FUNCTIONS_URL}/v1/posts/list`, {
-    method: 'POST',
-    body: JSON.stringify({ option }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    signal: ctrl?.signal,
-  });
-  const data = await response.json();
-  return data.map((item: any) => new Posts(item));
+  const { data: posts, error } = await supabase
+    .from('posts')
+    .select('*, profile(*)')
+    .eq('delete_flag', false)
+    .range(option.offset || 0, (option.limit || 10) + (option.offset || 0))
+    .order('created_at', { ascending: false });
+  if (error) {
+    throw error;
+  }
+
+  return posts;
 }
