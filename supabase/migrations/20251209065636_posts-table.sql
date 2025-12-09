7bbff5481067d5eb3b62b44f3b28af60c016cd0e
kiji-tech <shinji.tec.5761@gmail.com>
2025-12-09 15:56:55 +0900

    db update

---
+++ supabase/migrations/20251209065636_posts-table.sql
@@ -0,0 +1,29 @@
+grant delete on table "public"."posts" to "postgres";
+
+grant insert on table "public"."posts" to "postgres";
+
+grant references on table "public"."posts" to "postgres";
+
+grant select on table "public"."posts" to "postgres";
+
+grant trigger on table "public"."posts" to "postgres";
+
+grant truncate on table "public"."posts" to "postgres";
+
+grant update on table "public"."posts" to "postgres";
+
+grant delete on table "public"."posts_report" to "postgres";
+
+grant insert on table "public"."posts_report" to "postgres";
+
+grant references on table "public"."posts_report" to "postgres";
+
+grant select on table "public"."posts_report" to "postgres";
+
+grant trigger on table "public"."posts_report" to "postgres";
+
+grant truncate on table "public"."posts_report" to "postgres";
+
+grant update on table "public"."posts_report" to "postgres";
+
+
